b012bf3eaaf4cd5944c9cf90be80abb4f8203a9a
cemalettinaltintas <cemalettinaltintas@gmail.com>
2026-09-23 11:45:44 +0300

    Initial commit

---
+++ app/src/main/java/com/cemalettinaltintas/kotlinlearning/Main.kt
@@ -0,0 +1,90 @@
+package com.cemalettinaltintas.kotlinlearning
+
+    fun main(){
+        //Variables & Constants
+
+        //Integer
+
+        println("-------Integer-------")
+
+        var x=5
+        var y=4
+
+        println(x*y)
+
+
+        var age=35
+
+        val result=age/7
+
+        println(result)
+
+
+        //Defining
+        var myInteger: Int
+
+        //Initialize - Değer atama
+        myInteger=10
+
+        var myInteger2:Int=-20
+
+        println(myInteger2)
+
+        val a:Int=18
+
+        println(a)
+
+
+        var myByte: Byte=100
+
+
+        println(myByte)
+
+        var myShort: Short=1000
+
+        var myLong: Long=13403490925489058
+
+
+        //Double & Float
+
+        println("------------Double & Float------------")
+
+        val pi=3.14
+
+        val myFloat: Float=12.5f
+        println(myFloat)
+
+
+        val myDouble: Double=123.6
+
+        // Boolean
+
+        var evliMi: Boolean=true;
+
+        println(evliMi)
+
+        var isAlive=false
+
+        var sonuc: Boolean
+
+        sonuc=5>=5
+
+        println(sonuc)//false
+
+
+        //String
+        val myString="C. Altıntaş"
+
+        val name="Cemalettin"
+        val surname="Altintas"
+
+        val fullName=name+" "+surname
+
+
+        var departman: String="Human Resource"
+        println(departman)
+
+
+
+
+    }
